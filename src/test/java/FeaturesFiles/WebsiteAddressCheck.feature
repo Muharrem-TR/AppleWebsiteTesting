e8d00c2e0bb4d413ed241a0697e722da7917c750
Muharrem <m.karapazar2@gmail.com>
2022-11-15 17:36:21 +0300

    Seneryo Outline olarak güncellendi ve "Apple's Values","About Apple" eklendi.

--- src/test/java/FeaturesFiles/WebsiteAddressCheck.feature
+++ src/test/java/FeaturesFiles/WebsiteAddressCheck.feature
@@ -5,37 +5,78 @@ Feature: Website address check
     Then  Verify that you are on the right site
 
   @SmokeTest
-  Scenario: URL control of the links under the heading of Shopping and Detailed Information
-    Given Click store from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click Mac from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click Ipad from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click Iphone from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click watch from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click airpods from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click tv and home from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click airtag from the Apple Footer menu
-    When Check the url of the menus under Shopping and Detailed Information
-    Given Click accessories from the Apple Footer menu
+  Scenario Outline: URL control of links under Services, Shopping and Detailed Information
+
+    Given URL control of the links under the heading of "<Shopping and Detailed Information>"
     When Check the url of the menus under Shopping and Detailed Information
 
-  @SmokeTest
-  Scenario: URL control of the links under the Services heading
-    Given Click apple music from the Apple Footer menu
+    Given URL control of the links under the "<Services>" heading
     When Check the url of the menus under services
-    Given Click apple arcade from the Apple Footer menu
-    When Check the url of the menus under services
-    Given Click iCloud from the Apple Footer menu
-    When Check the url of the menus under services
-    Given Click apple books from the Apple Footer menu
-    When Check the url of the menus under services
-    Given Click apple podcasts from the Apple Footer menu
-    When Check the url of the menus under services
-    Given Click app store from the Apple Footer menu
-    When Check the url of the menus under services
+
+    Given URL checking of links under "<Apple's Values>"
+    When Check the url of the menus under Apple's Values
+
+    Given URL check of links under "<About Apple>"
+    When Check the url of the menus under About Apple
+
+    Examples:
+      | Shopping and Detailed Information | Services                  | Apple's Values                     | About Apple                     |
+      | altBilgiMenuStore                 | altBilgiMenuAppleMusic    | altBilgiMenuAccessibility          | altBilgiMenuNewsroom            |
+      | altBilgiMenuMac                   | altBilgiMenuAppleArcade   | altBilgiMenuEnvironment            | altBilgiMenuAppleLeadership     |
+      | altBilgiMenuIpad                  | altBilgiMenuICloud        | altBilgiMenuPrivacy                | altBilgiMenuCareerOpportunities |
+      | altBilgiMenuIphone                | altBilgiMenuAppleBooks    | altBilgiMenuSupplierResponsibility | altBilgiMenuWarranty            |
+      | altBilgiMenuWatch                 | altBilgiMenuApplePodcasts | pass                               | altBilgiMenuEthicsAndCompliance |
+      | altBilgiMenuAirpods               | altBilgiMenuAppleStore    | pass                               | altBilgiMenuEvents              |
+      | altBilgiMenuTvandHome             | pass                      | pass                               | altBilgiMenuContactApple        |
+      | altBilgiMenuAirtag                | pass                      | pass                               | pass                            |
+      | altBilgiMenuAccessories           | pass                      | pass                               | pass                            |
+
+#   Veri olmayan yerlere "pass" yazılmalıdır. Bu sayede Test ve Doğrulama pas geçilecektir. Aksi taktirde hata alınır.
+
+
+
+
+
+
+
+
+
+
+
+
+#  -------------------------------  Uzun ve Karmaşık Hali  -------------------------------
+#  @SmokeTest
+#  Scenario: URL control of the links under the heading of Shopping and Detailed Information
+#    Given Click store from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click Mac from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click Ipad from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click Iphone from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click watch from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click airpods from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click tv and home from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click airtag from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#    Given Click accessories from the Apple Footer menu
+#    When Check the url of the menus under Shopping and Detailed Information
+#
+#  @SmokeTest
+#  Scenario: URL control of the links under the Services heading
+#    Given Click apple music from the Apple Footer menu
+#    When Check the url of the menus under services
+#    Given Click apple arcade from the Apple Footer menu
+#    When Check the url of the menus under services
+#    Given Click iCloud from the Apple Footer menu
+#    When Check the url of the menus under services
+#    Given Click apple books from the Apple Footer menu
+#    When Check the url of the menus under services
+#    Given Click apple podcasts from the Apple Footer menu
+#    When Check the url of the menus under services
+#    Given Click app store from the Apple Footer menu
+#    When Check the url of the menus under services
--- src/test/java/StepDefinitions/WebsiteAddressCheckSteps.java
+++ src/test/java/StepDefinitions/WebsiteAddressCheckSteps.java
@@ -7,13 +7,8 @@ import io.cucumber.java.en.When;
 import junit.framework.Assert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+
 
 public class WebsiteAddressCheckSteps {
 
@@ -21,118 +16,225 @@ public class WebsiteAddressCheckSteps {
 
     WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
 
-    @Given("Click store from the Apple Footer menu")
-    public void clickStoreFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuStore");
+    String passGec="pass";
+
+    @Given("URL control of the links under the heading of {string}")
+    public void urlControlOfTheLinksUnderTheHeadingOf(String ShoppingAndDetailedInformation) {
+
+        if (passGec.equals(ShoppingAndDetailedInformation)){
+            passGec="passGecildi";
+        }else {
+            dc.findAndClick(ShoppingAndDetailedInformation);
+        }
+    }
+
+    @Given("URL control of the links under the {string} heading")
+    public void urlControlOfTheLinksUnderTheHeading(String Services) {
+
+        if (passGec.equals(Services)){
+            passGec="passGecildi";
+        }else {
+            dc.findAndClick(Services);
+        }
     }
 
     @When("Check the url of the menus under Shopping and Detailed Information")
     public void checkTheUrlOfTheMenusUnderShoppingAndDetailedInformation() {
-        String[] urller={"store","mac","ipad","iphone","watch","airpods","tv-home","airtag","accessories"};
 
-        wait.until(ExpectedConditions.urlContains("apple"));
-        String siteURL= GWD.getDriver().getCurrentUrl();
+        if (passGec.equals("passGecildi")){
+            passGec="pass";
+        }else {
 
-        int yanlis=0; int donguSayisi=0;
-        for (int i = 0; i < urller.length; i++) {
-            donguSayisi++;
-            if (siteURL.contains(urller[i])){
-                break;
-            }else {
-                yanlis++;
+            String[] urller={"store","mac","ipad","iphone","watch","airpods","tv-home","airtag","accessories"};
+
+            wait.until(ExpectedConditions.urlContains("apple"));
+            String siteURL= GWD.getDriver().getCurrentUrl();
+
+            int yanlis=0; int donguSayisi=0;
+            for (int i = 0; i < urller.length; i++) {
+                donguSayisi++;
+                if (siteURL.contains(urller[i])){
+                    break;
+                }else {
+                    yanlis++;
+                }
             }
+            Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
+            dc.findAndClick("anaSayfa");
         }
-        Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
-        dc.findAndClick("anaSayfa");
-    }
-
-    @Given("Click Mac from the Apple Footer menu")
-    public void clickMacFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuMac");
-    }
-
-    @Given("Click Ipad from the Apple Footer menu")
-    public void clickIpadFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuIpad");
-    }
-
-    @Given("Click Iphone from the Apple Footer menu")
-    public void clickIphoneFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuIphone");
-    }
-
-    @Given("Click watch from the Apple Footer menu")
-    public void clickWatchFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuWatch");
-    }
-
-    @Given("Click airpods from the Apple Footer menu")
-    public void clickAirpodsFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAirpods");
-    }
-
-    @Given("Click tv and home from the Apple Footer menu")
-    public void clickTvAndHomeFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuTvandHome");
-    }
-
-    @Given("Click airtag from the Apple Footer menu")
-    public void clickAirtagFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAirtag");
-    }
-
-    @Given("Click accessories from the Apple Footer menu")
-    public void clickAccessoriesFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAccessories");
-    }
-
-    @Given("Click apple music from the Apple Footer menu")
-    public void clickAppleMusicFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAppleMusic");
     }
 
     @When("Check the url of the menus under services")
     public void checkTheUrlOfTheMenusUnderServices() {
-        String[] urller={"apple-music","apple-arcade","icloud","apple-books","apple-podcasts","app-store"};
 
-        wait.until(ExpectedConditions.urlContains("apple"));
-        String siteURL= GWD.getDriver().getCurrentUrl();
+        if (passGec.equals("passGecildi")){
+            passGec="pass";
+        }else {
 
-        int yanlis=0; int donguSayisi=0;
-        for (int i = 0; i < urller.length; i++) {
-            donguSayisi++;
-            if (siteURL.contains(urller[i])){
-                break;
-            }else {
-                yanlis++;
+            String[] urller={"apple-music","apple-arcade","icloud","apple-books","apple-podcasts","app-store"};
+
+            wait.until(ExpectedConditions.urlContains("apple"));
+            String siteURL= GWD.getDriver().getCurrentUrl();
+
+            int yanlis=0; int donguSayisi=0;
+            for (int i = 0; i < urller.length; i++) {
+                donguSayisi++;
+                if (siteURL.contains(urller[i])){
+                    break;
+                }else {
+                    yanlis++;
+                }
             }
+            Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
+            dc.findAndClick("anaSayfa");
         }
-        Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
-        dc.findAndClick("anaSayfa");
     }
 
-    @Given("Click apple arcade from the Apple Footer menu")
-    public void clickAppleArcadeFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAppleArcade");
+    @Given("URL checking of links under {string}")
+    public void urlCheckingOfLinksUnder(String AppleValues) {
+
+        if (passGec.equals(AppleValues)){
+            passGec="passGecildi";
+        }else {
+            dc.findAndClick(AppleValues);
+        }
     }
 
-    @Given("Click iCloud from the Apple Footer menu")
-    public void clickICloudFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuiCloud");
+    @When("Check the url of the menus under Apple's Values")
+    public void checkTheUrlOfTheMenusUnderAppleSValues() {
+
+        if (passGec.equals("passGecildi")){
+            passGec="pass";
+        }else {
+
+            String[] urller={"accessibility","environment","privacy","supplier-responsibility"};
+
+            wait.until(ExpectedConditions.urlContains("apple"));
+            String siteURL= GWD.getDriver().getCurrentUrl();
+
+            int yanlis=0; int donguSayisi=0;
+            for (int i = 0; i < urller.length; i++) {
+                donguSayisi++;
+                if (siteURL.contains(urller[i])){
+                    break;
+                }else {
+                    yanlis++;
+                }
+            }
+            Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
+            dc.findAndClick("anaSayfa");
+        }
     }
 
-    @Given("Click apple books from the Apple Footer menu")
-    public void clickAppleBooksFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAppleBooks");
+    @Given("URL check of links under {string}")
+    public void urlCheckOfLinksUnder(String AboutApple) {
+
+        if (passGec.equals(AboutApple)){
+            passGec="passGecildi";
+        }else {
+            dc.findAndClick(AboutApple);
+        }
     }
 
-    @Given("Click apple podcasts from the Apple Footer menu")
-    public void clickApplePodcastsFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuApplePodcasts");
+    @When("Check the url of the menus under About Apple")
+    public void checkTheUrlOfTheMenusUnderAboutApple() {
+
+        if (passGec.equals("passGecildi")){
+            passGec="pass";
+        }else {
+
+            String[] urller={"newsroom","leadership","careers","warranty","compliance","apple-events","contact"};
+
+            wait.until(ExpectedConditions.urlContains("apple"));
+            String siteURL= GWD.getDriver().getCurrentUrl();
+
+            int yanlis=0; int donguSayisi=0;
+            for (int i = 0; i < urller.length; i++) {
+                donguSayisi++;
+                if (siteURL.contains(urller[i])){
+                    break;
+                }else {
+                    yanlis++;
+                }
+            }
+            Assert.assertTrue("Hata: Site doğrulanamadı. Eşleşen URL yok.",!(donguSayisi==yanlis));
+            dc.findAndClick("anaSayfa");
+        }
     }
 
-    @Given("Click app store from the Apple Footer menu")
-    public void clickAppStoreFromTheAppleFooterMenu() {
-        dc.findAndClick("altBilgiMenuAppleStore");
-    }
+//    @Given("Click store from the Apple Footer menu")
+//    public void clickStoreFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuStore");
+//    }
+//
+//    @Given("Click Mac from the Apple Footer menu")
+//    public void clickMacFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuMac");
+//    }
+//
+//    @Given("Click Ipad from the Apple Footer menu")
+//    public void clickIpadFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuIpad");
+//    }
+//
+//    @Given("Click Iphone from the Apple Footer menu")
+//    public void clickIphoneFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuIphone");
+//    }
+//
+//    @Given("Click watch from the Apple Footer menu")
+//    public void clickWatchFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuWatch");
+//    }
+//
+//    @Given("Click airpods from the Apple Footer menu")
+//    public void clickAirpodsFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAirpods");
+//    }
+//
+//    @Given("Click tv and home from the Apple Footer menu")
+//    public void clickTvAndHomeFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuTvandHome");
+//    }
+//
+//    @Given("Click airtag from the Apple Footer menu")
+//    public void clickAirtagFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAirtag");
+//    }
+//
+//    @Given("Click accessories from the Apple Footer menu")
+//    public void clickAccessoriesFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAccessories");
+//    }
+//
+//    @Given("Click apple music from the Apple Footer menu")
+//    public void clickAppleMusicFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAppleMusic");
+//    }
+//
+//    @Given("Click apple arcade from the Apple Footer menu")
+//    public void clickAppleArcadeFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAppleArcade");
+//    }
+//
+//    @Given("Click iCloud from the Apple Footer menu")
+//    public void clickICloudFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuiCloud");
+//    }
+//
+//    @Given("Click apple books from the Apple Footer menu")
+//    public void clickAppleBooksFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAppleBooks");
+//    }
+//
+//    @Given("Click apple podcasts from the Apple Footer menu")
+//    public void clickApplePodcastsFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuApplePodcasts");
+//    }
+//
+//    @Given("Click app store from the Apple Footer menu")
+//    public void clickAppStoreFromTheAppleFooterMenu() {
+//        dc.findAndClick("altBilgiMenuAppleStore");
+//    }
+
 }
